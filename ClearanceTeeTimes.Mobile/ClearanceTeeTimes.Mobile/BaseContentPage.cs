537fc65f1f6601e789388ce615b2c8025ad6cddf
Kyle Lutes <Kyle Lutes>
2016-03-30 10:13:39 -0400

    Interface and viewmodel for authentication

---
+++ ClearanceTeeTimes.Mobile/ClearanceTeeTimes.Mobile/BaseContentPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ClearanceTeeTimes.Mobile
+{
+    public class BaseContentPage : ContentPage
+    {
+        public BaseContentPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
---
+++ ClearanceTeeTimes.Mobile/ClearanceTeeTimes.Mobile/Interfaces/IAuthentication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearanceTeeTimes.Mobile.Interfaces
+{
+    public interface IAuthentication
+    {
+
+    }
+}
