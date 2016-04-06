d77b1280667e24a8b1b1d147c03e6350d825289b
Chris Olson <chris.olson@plumreward.com>
2016-04-06 13:32:10 -0400

    finished get methods for user information

--- ClearanceTeeTimes.Mobile/ClearanceTeeTimes.Mobile/Pages/RegisterInfo/UserRegisterInfo.xaml.cs
+++ ClearanceTeeTimes.Mobile/ClearanceTeeTimes.Mobile/Pages/RegisterInfo/UserRegisterInfo.xaml.cs
@@ -15,8 +15,8 @@ namespace ClearanceTeeTimes.Mobile
 		public string GetUserFirstName(string fName)
 		{
 			if (fNameEntryBox.Text == null) {
-				DisplayAlert ("Error", "First name cannot be null! Please re-enter.");
-				return;
+				DisplayAlert ("Error", "First name cannot be empty! Please re-enter.", "Ok");
+				return fName;
 			} else {
 				fName = fNameEntryBox.Text;
 			}
@@ -26,14 +26,69 @@ namespace ClearanceTeeTimes.Mobile
 		public string GetUserLastName(string lName)
 		{
 			if (lNameEntryBox.Text == null) {
-				DisplayAlert ("Error", "Last name cannot be null! Please re-enter.");
-				return;
+				DisplayAlert ("Error", "Last name cannot be empty! Please re-enter.", "Ok");
+				return lName;
 			} else {
-				fName = fNameEntryBox.Text;
+				lName = lNameEntryBox.Text;
 			}
-			return fName;
+			return lName;
 		}
 
+
+		public string GetUserSex(string mf)
+		{
+			if (maleFemaleEntryBox.Text == null) {
+				DisplayAlert ("Error", "Sex cannot be empty! Please re-enter.", "Ok");
+				return mf;
+			} else {
+				mf = maleFemaleEntryBox.Text;
+			}
+			return mf;
+		}
+
+		public string GetUserCity(string city)
+		{
+			if (cityEntryBox.Text == null) {
+				DisplayAlert ("Error", "City cannot be empty! Please re-enter.", "Ok");
+				return city;
+			} else {
+				city = cityEntryBox.Text;
+			}
+			return city;
+		}
+
+		public string GetUserState(string state)
+		{
+			if (stateEntryBox.Text == null) {
+				DisplayAlert ("Error", "State cannot be empty! Please re-enter.", "Ok");
+				return state;
+			} else {
+				state = stateEntryBox.Text;
+			}
+			return state;
+		}
+
+		public string GetUserZip(string zip)
+		{
+			if (zipEntryBox.Text == null) {
+				DisplayAlert ("Error", "Zip code cannot be empty! Please re-enter.", "Ok");
+				return zip;
+			} else {
+				zip = zipEntryBox.Text;
+			}
+			return zip;
+		}
+
+		public string GetUserEmail(string email)
+		{
+			if (emailEntryBox.Text == null) {
+				DisplayAlert ("Error", "Email cannot be empty! Please re-enter.", "Ok");
+				return email;
+			} else {
+				email = emailEntryBox.Text;
+			}
+			return email;
+		}
 
 
 	}
