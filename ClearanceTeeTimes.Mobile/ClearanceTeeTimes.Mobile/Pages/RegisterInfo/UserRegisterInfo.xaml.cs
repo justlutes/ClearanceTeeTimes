d9f853e90e4dd959463dd5e8a16f60c49be608d8
Chris Olson <chris.olson@plumreward.com>
2016-04-06 11:54:51 -0400

    Added the registration page skeleton, guts next

---
+++ ClearanceTeeTimes.Mobile/ClearanceTeeTimes.Mobile/Pages/RegisterInfo/UserRegisterInfo.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ClearanceTeeTimes.Mobile
+{
+	public partial class UserRegisterInfo : ContentPage
+	{
+		public UserRegisterInfo ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
