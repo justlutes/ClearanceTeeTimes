841fe3a5101001316ec5d9defc8fc7a4a632cdc3
Chris Olson <chris.olson@plumreward.com>
2016-04-06 12:48:35 -0400

    methods added

--- ClearanceTeeTimes.Mobile/ClearanceTeeTimes.Mobile/Pages/RegisterInfo/UserRegisterInfo.xaml.cs
+++ ClearanceTeeTimes.Mobile/ClearanceTeeTimes.Mobile/Pages/RegisterInfo/UserRegisterInfo.xaml.cs
@@ -11,6 +11,31 @@ namespace ClearanceTeeTimes.Mobile
 		{
 			InitializeComponent ();
 		}
+
+		public string GetUserFirstName(string fName)
+		{
+			if (fNameEntryBox.Text == null) {
+				DisplayAlert ("Error", "First name cannot be null! Please re-enter.");
+				return;
+			} else {
+				fName = fNameEntryBox.Text;
+			}
+			return fName;
+		}
+
+		public string GetUserLastName(string lName)
+		{
+			if (lNameEntryBox.Text == null) {
+				DisplayAlert ("Error", "Last name cannot be null! Please re-enter.");
+				return;
+			} else {
+				fName = fNameEntryBox.Text;
+			}
+			return fName;
+		}
+
+
+
 	}
 }
-
+//ohhh i see//ok interesting ..
