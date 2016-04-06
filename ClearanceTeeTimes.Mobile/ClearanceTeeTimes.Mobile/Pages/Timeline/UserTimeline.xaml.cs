901281f4bb6b05d929103a1699450a0d88f2618a
Chris Olson <chris.olson@plumreward.com>
2016-04-06 14:34:42 -0400

    Created user timeline page

---
+++ ClearanceTeeTimes.Mobile/ClearanceTeeTimes.Mobile/Pages/Timeline/UserTimeline.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ClearanceTeeTimes.Mobile
+{
+	public partial class UserTimeline : ContentPage
+	{
+		public UserTimeline ()
+		{
+			InitializeComponent ();
+		}
+
+		static string selectUserCourses = "";
+
+		public List<string> GetUserCourses(string selectCourses)
+		{
+			List<string> userCourses;
+
+
+			return userCourses;
+		}
+
+	}
+}
+
