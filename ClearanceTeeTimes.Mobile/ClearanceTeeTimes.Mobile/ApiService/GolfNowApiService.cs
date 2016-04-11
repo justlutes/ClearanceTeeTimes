7b9081d8e108d1f7805cf41e3dd3245ffc6baaa2
Chris Olson <chris.olson@plumreward.com>
2016-04-11 14:52:11 -0400

    Added guts to ApiService

---
+++ ClearanceTeeTimes.Mobile/ClearanceTeeTimes.Mobile/ApiService/GolfNowApiService.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Net.Http;
+using Fusillade;
+using ModernHttpClient;
+using Refit;
+using System.Threading.Tasks;
+using GolfNow.Mobile.Core.Business;
+using GolfNow.Mobile.Business;
+using GolfNow.Mobile.Core.Api;
+using Newtonsoft.Json;
+
+namespace ClearanceTeeTimes.Mobile
+{
+	[Preserve (AllMembers = true)]
+
+	public class GolfNowApiService
+	{
+		//public const string ApiBaseAddress = "https://rcapi.gnsvc.com/rest";
+		//public const string ApiBaseAddress = "https://2-1-17.devapi.gnsvc.com/rest";
+		public const string ApiBaseAddress = "https://2-1-17-sandbox.api.gnsvc.com/rest";
+		//public const string ApiBaseAddress = "https://2-1-17.api.gnsvc.com/rest";
+		private readonly Lazy<IMobileGolfNowApi> _background;
+		private readonly Lazy<IMobileGolfNowApi> _speculative;
+		private readonly Lazy<IMobileGolfNowApi> _userInitiated;
+
+		public string ApiPassword;
+		public string ApiUser;
+
+
+
+		public GolfNowApiService (string userName, string passWord, string apiBaseAddress = null)
+		{ApiUser = userName;
+			ApiPassword = passWord;
+
+			Func<HttpMessageHandler, IMobileGolfNowApi> createClient = messageHandler => {
+				var client = new HttpClient (messageHandler) {
+					BaseAddress = new Uri (apiBaseAddress)
+				};
+
+				client.DefaultRequestHeaders.Add ("UserName", userName);
+				client.DefaultRequestHeaders.Add ("Password", passWord);
+
+
+				return RestService.For<IMobileGolfNowApi> (client, new RefitSettings()
+					{
+
+						JsonSerializerSettings = new Newtonsoft.Json.JsonSerializerSettings()
+						{
+							Converters = {new SavedPaymentConverter()},
+							TypeNameHandling = TypeNameHandling.Objects
+						},
+					});
+			};
+
+			_background = new Lazy<IMobileGolfNowApi> (() => createClient (
+				new RateLimitedHttpMessageHandler (new GolfNowApiAuthenticationHandler (), Priority.Background)));
+
+			_userInitiated = new Lazy<IMobileGolfNowApi> (() => createClient (
+				new RateLimitedHttpMessageHandler (new GolfNowApiAuthenticationHandler (), Priority.UserInitiated)));
+
+			_speculative = new Lazy<IMobileGolfNowApi> (() => createClient (
+				new RateLimitedHttpMessageHandler (new GolfNowApiAuthenticationHandler (), Priority.Speculative)));
+
+			//TODO: Xamarin : add a check for connection before making the requests.
+			Task.Run (async () => await InitiateClients ());
+		}
+
+		private async Task InitiateClients ()
+		{
+			try {
+				await _background.Value.SecureEcho ("I");
+				await _userInitiated.Value.SecureEcho ("I");
+				await _speculative.Value.SecureEcho ("I");
+			} catch (Exception ex) {
+
+				ExceptionService.PublishEx (ex);
+
+			}
+
+
+		}
+
+
+		[Preserve]
+		public IMobileGolfNowApi Background {
+			get { return _background.Value; }
+		}
+
+		[Preserve]
+		public IMobileGolfNowApi UserInitiated {
+			get { return _userInitiated.Value; }
+		}
+
+		[Preserve]
+		public IMobileGolfNowApi Speculative {
+			get { return _speculative.Value; }
+		}
+
+
+		public IMobileGolfNowApi GetPriorityApi (Priority priority)
+		{
+			IMobileGolfNowApi api;
+			switch (priority) {
+			case Priority.Background:
+				api = this.Background;
+				break;
+			case Priority.UserInitiated:
+				api = this.UserInitiated;
+				break;
+			case Priority.Speculative:
+				api = this.Speculative;
+				break;
+			default:
+				api = this.UserInitiated;
+				break;
+			}
+
+			return api;
+
+		}
+	}
+}
+
